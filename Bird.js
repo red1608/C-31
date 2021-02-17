5bdb3f5889153f207865080fbbccbda9f364fdcc
red1608 <66174494+red1608@users.noreply.github.com>
2021-02-17 15:59:17 -0500

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,24 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smokeimage = loadImage("sprites/smoke.png");
+    this.trajectory = [];
+    
+    
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    if (this.body.position.x>220&&this.body.speed>10){
+  this.position = [this.body.position.x,this.body.position.y];
+    
+  this.trajectory.push(this.position);
+    }
+   for (var i=0;i<this.trajectory.length;i++){
+     image (this.smokeimage,this.trajectory[i][0],this.trajectory[i][1])
+   }
+    super.display();
+  }
+}
